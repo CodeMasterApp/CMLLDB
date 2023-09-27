f29a99b5da3ec1c4607cbae71eedecdea170e9c3
N3v1 <n3v1010@gmail.com>
2023-09-27 20:23:55 +0200

    config

---
+++ CMLLDB/Tests/CMLLDBTests/CMLLDBTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import CMLLDB
+
+final class CMLLDBTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
